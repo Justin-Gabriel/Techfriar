534cff8e61c429a04b5e21568594c28502ff642d
Justin-Gabriel <gabriel.justin78@gmail.com>
2024-07-26 18:13:36 +0530

    feat: start page

---
+++ src/pages/Start.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header.jsx';
+import CompanySection from '../components/CompanySection.jsx';
+const Start2 = () => {
+
+    return (
+        <div className="h-[919px] bg-gradient-to-r from-blue-400 to-pink-300">
+            <Header />
+            <CompanySection />
+        </div>
+
+    )
+} 
+
+export default Start2
